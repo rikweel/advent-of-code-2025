3fa36c58cc9a411ac4bd8748e9c3f7faf99d8716
Rik Weel <rikweel@gmail.com>
2025-12-01 20:51:29 +0100

    Refactor into class.

--- day-1/secret_entrance.rb
+++ day-1/secret_entrance.rb
@@ -1,16 +1,35 @@
-rotations = File.read('rotations.txt').split("\n")
-dial = Array.new(100) { |i| i }
-dial.rotate!(50)
-
-@answer = 0
-rotations.each do |rotation|
-  if rotation[0] == 'L'
-    dial.rotate!(rotation[1..-1].to_i)
-  else
-    dial.rotate!(-rotation[1..-1].to_i)
+class SecretEntranceOpener
+  def initialize(rotations, dial_length = 100, starting_position = 50)
+    @rotations = rotations
+    @dial_length = dial_length
+    @starting_position = starting_position
   end
 
-  dial[0] == 0 && @answer += 1
+  def create_dial
+    dial = Array.new(@dial_length) { |i| i } 
+    dial.rotate!(@starting_position)
+    return dial
+  end
+
+  def solve_secret_code
+    answer = 0
+    dial = create_dial
+
+    @rotations.each do |rotation|
+      if rotation[0] == 'L'
+        dial.rotate!(rotation[1..-1].to_i)
+      else
+        dial.rotate!(-rotation[1..-1].to_i)
+      end
+
+      dial[0] == 0 && answer += 1
+    end
+
+    answer
+  end
 end
 
-puts @answer
+rotations = File.read('rotations.txt').split("\n")
+secret_entrance = SecretEntranceOpener.new(rotations)
+
+puts secret_entrance.solve_secret_code
