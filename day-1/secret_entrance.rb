3b4e75b8f2c33d9b1bd610ba751060c664bf79f9
Rik Weel <rikweel@gmail.com>
2025-12-02 23:41:55 +0100

    day 2

--- day-1/secret_entrance.rb
+++ day-1/secret_entrance.rb
@@ -11,7 +11,7 @@ class SecretEntranceOpener
     return dial
   end
 
-  def solve_secret_code
+  def solve_secret_code_1
     answer = 0
     dial = create_dial
 
@@ -22,7 +22,43 @@ class SecretEntranceOpener
         dial.rotate!(-rotation[1..-1].to_i)
       end
 
-      dial[0] == 0 && answer += 1
+      dial[0].zero? && answer += 1
+    end
+
+    answer
+  end
+
+  def count_points_at_zero(position, total_rotations)
+    count = 0
+    full_rotations = ((position.to_f + total_rotations) / @dial_length).abs.floor
+
+    count += full_rotations
+
+    if ((position + total_rotations) % @dial_length == 0 && count > 0) 
+      count -= 1
+    end
+
+    if full_rotations.abs == 0 && (position + total_rotations < 0)
+      count += 1
+    end
+
+    count
+  end
+
+  def solve_secret_code_2
+    answer = 0
+    dial = create_dial
+
+    @rotations.each do |rotation|
+      if rotation[0] == 'L'
+        answer += count_points_at_zero(dial[0], rotation[1..-1].to_i)
+        dial.rotate!(rotation[1..-1].to_i)
+      else
+        answer += count_points_at_zero(dial[0], -rotation[1..-1].to_i)
+        dial.rotate!(-rotation[1..-1].to_i)
+      end
+
+      dial[0].zero? && answer += 1
     end
 
     answer
@@ -32,4 +68,4 @@ end
 rotations = File.read('rotations.txt').split("\n")
 secret_entrance = SecretEntranceOpener.new(rotations)
 
-puts secret_entrance.solve_secret_code
+puts secret_entrance.solve_secret_code_2
--- day-2/gift_shop.rb
+++ day-2/gift_shop.rb
@@ -1,13 +1,31 @@
 input = File.read('day-2/input.txt').split(',').map { |range| range.split('-').map(&:to_i) }
 
+def repeated_sequence?(arr)
+  return false if arr.empty? || arr.length.odd?
+  
+  mid = arr.length / 2
+  arr[0...mid] == arr[mid..-1]
+end
+
+def repeated_sequence_two?(arr)
+  return false if arr.empty?
+  
+  (1..arr.length/2).each do |seq_len|
+    next unless arr.length % seq_len == 0
+    
+    sequence = arr[0...seq_len]
+    return true if arr.each_slice(seq_len).all? { |chunk| chunk == sequence }
+  end
+  
+  false
+end
+
 def find_invalid_ids(input)
   answer = 0
   for range in input
     all_numbers = (range[0]..range[1]).to_a
     for number in all_numbers
-      digits = number.digits
-      # TODO: add invalid numbers, not all numers
-      answer += number unless digits.uniq.length == digits.length
+      repeated_sequence_two?(number.digits.reverse) && answer += number
     end
   end
 
