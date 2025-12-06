06637794150849b6445891d14ab6f425ead25ce1
Rik Weel <rikweel@gmail.com>
2025-12-06 13:49:49 +0100

    day 3.

--- day-3/lobby.rb
+++ day-3/lobby.rb
@@ -1,26 +1,44 @@
 input = File.read('day-3/input.txt').split("\n")
 
-def find_max_joltage_per_bank(bank)
-  bank_arr = bank.each_char.map(&:to_i)
+def find_max_joltage_per_bank(bank, n)
+  digits = bank.each_char.map(&:to_i)
+  max_joltage = 0
 
-  first_number = bank_arr.max
-  first_index = bank_arr.index(first_number)
+  (0..digits.length - n).each do |first_index|
+    selected = [digits[first_index]]
+    current_index = first_index
 
-  if first_index == bank_arr.length - 1
-    second_number = first_number
-    first_number = bank_arr[0...first_index].max
-  else 
-    second_number = bank_arr[(first_index + 1)..-1].max
+    (n - 1).times do
+      remaining_needed = n - selected.length
+      remaining_start = current_index + 1
+      search_end = digits.length - remaining_needed
+
+      break if search_end < remaining_start
+
+      segment = digits[remaining_start..search_end]
+      break if segment.nil? || segment.empty?
+
+      next_digit = segment.max
+      next_index = segment.index(next_digit) + remaining_start
+
+      selected << next_digit
+      current_index = next_index
+    end
+
+    if selected.length == n
+      joltage = selected.join.to_i
+      max_joltage = [max_joltage, joltage].max
+    end
   end
 
-  [first_number, second_number].join.to_i
+  max_joltage
 end
 
 def find_max_total_joltage(input)
   total_joltage = 0
 
   input.each do |bank|
-    max_joltage = find_max_joltage_per_bank(bank)
+    max_joltage = find_max_joltage_per_bank(bank, 12)
     total_joltage += max_joltage
   end
 
